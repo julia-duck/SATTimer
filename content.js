3585c01c1705edc31e6826e3e200ba29f0e5abc8
Julia Jeng <princessmisty48@gmail.com>
2026-07-05 18:00:06 -0400

    Added stopwatch when timer runs out as well as summary button that gives alert of how many questions you completed in how much time

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
 {
     let displayStr = "00:00";
     let secs = 0;
+    let questions = 0;
+    let totalTime = 0;
+    let time = 0;
     if (window.timerId) {
         clearInterval(window.timerId);
     }
@@ -16,6 +19,8 @@
             <div id="buttons">
                 <button id="easy" class="button">Easy</button>
                 <button id="hard" class="button">Hard</button>
+                <button id="done" class="button">Done</button>
+                <button id="summary" class="button">Summary</button>
             </div>
         `;
 
@@ -30,13 +35,28 @@
         });
 
         document.getElementById("easy").addEventListener("click", () => {
+            time = 0;
             secs = 60;
             setTimer();
         });
 
         document.getElementById("hard").addEventListener("click", () => {
+            time = 0;
             secs = 120;
             setTimer();
+        });
+
+        document.getElementById("done").addEventListener("click", () => {
+            questions++;
+            clearInterval(window.timerId);
+            resetTimer();
+            totalTime += time;
+        });
+
+        document.getElementById("summary").addEventListener("click", () => {
+            let mins = Math.floor(totalTime / 60);
+            let rightSecs = totalTime % 60;
+            alert(questions + " questions in " + mins + " minutes and " + rightSecs + " seconds");
         });
 
         document.getElementById("close-button").addEventListener("click", CloseTimer);
@@ -47,6 +67,7 @@
 
 
     function setTimer() {
+        document.getElementById("timer-display").dataset.state = "countdown";
         if (window.timerId) {
             clearInterval(window.timerId);
         }
@@ -57,14 +78,39 @@
     function countdown() {
         if (secs == -1) {
             clearInterval(window.timerId);
+            startStopwatch();
             return;
         }
+        time += 1;
         let mins = Math.floor(secs / 60);
         let rightSecs = secs % 60;
         const displayMins = mins.toString().padStart(2, '0');
         const displaySecs = rightSecs.toString().padStart(2, '0');
         displayStr = displayMins + ":" + displaySecs;
         secs--;
+        document.getElementById("timer-display").innerText = displayStr;
+    }
+
+    function startStopwatch() {
+        secs = 0;
+        document.getElementById("timer-display").dataset.state = "countup";
+        countup();
+        window.timerId = setInterval(() => {countup();}, 1000);
+    }
+
+    function countup() {
+        time += 1;
+        let mins = Math.floor(secs / 60);
+        let rightSecs = secs % 60;
+        const displayMins = mins.toString().padStart(2, '0');
+        const displaySecs = rightSecs.toString().padStart(2, '0');
+        displayStr = "-" + displayMins + ":" + displaySecs;
+        secs++;
+        document.getElementById("timer-display").innerText = displayStr;
+    }
+
+    function resetTimer() {
+        displayStr = "00:00";
         document.getElementById("timer-display").innerText = displayStr;
     }
 
